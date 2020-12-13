91e3e61c0813fae55b2b5a5aefa040abf3281f5b
rurikjart <rurik.j.art@gmail.com>
2020-12-13 21:27:53 +0300

    Task 4. Создаем Toolbar (Часть 1)

--- app/src/main/java/org/ut/rme/MainActivity.java
+++ app/src/main/java/org/ut/rme/MainActivity.java
@@ -2,12 +2,36 @@ package org.ut.rme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // точка входа для логики, создаем тул бар
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+
     }
 }
