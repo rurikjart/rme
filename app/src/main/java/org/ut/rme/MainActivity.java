971adbcc2189158334ee50992c559431100fc611
rurikjart <rurik.j.art@gmail.com>
2020-12-17 21:56:00 +0300

    Task 4. Создаем NavigationView

--- app/src/main/java/org/ut/rme/MainActivity.java
+++ app/src/main/java/org/ut/rme/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_Default);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
