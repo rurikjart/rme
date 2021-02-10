37d15eedc06110c8aac649953544353ffe60612d
rurikjart <rurik.j.art@gmail.com>
2021-02-10 21:22:54 +0300

    Task 7. NavigationView добавляем содержимое

--- app/src/main/java/org/ut/rme/MainActivity.java
+++ app/src/main/java/org/ut/rme/MainActivity.java
@@ -1,5 +1,6 @@
 package org.ut.rme;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,18 +8,22 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme_Default);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         // точка входа для логики, создаем тул бар
         initToolbar();
-
+        initNavigationView();
     }
+
+
 
     private void initToolbar() {
 
@@ -34,5 +39,9 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar.inflateMenu(R.menu.menu);
 
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
